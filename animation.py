4757b55ed268b2bfd37a09c8b227f89530e37824
Paulina Hering <paulina.hering@outlook.de>
2021-10-21 20:21:07 +0200

    Some Circles with straight Lines and a first try of a Geodesic

--- animation.py
+++ animation.py
@@ -12,3 +12,30 @@ class SquareToCircle(Scene):
         self.play(Create(square))  # animate the creation of the square
         self.play(Transform(square, circle))  # interpolate the square into the circle
         self.play(FadeOut(square))  # fade out animation
+
+class CircleAndStraightLine(Scene):
+    def construct(self):
+        circle = Circle() #create circle
+        line1 = Line() #creates line
+
+        self.play(Create(circle))
+        self.play(Create(line1))
+
+class CirclesNextToEachOther(Scene):
+    def construct(self):
+        circle1 = Circle()
+        circle2 = Circle()
+        circle2.move_to([2, 0, 0]) #moves circle2 to the right by 2
+
+        self.play(Create(circle1))
+        self.play(Create(circle2))
+
+class CircleAndGeodesic(Scene):
+    def construct(self):
+        circle1 = Circle()
+        geod = Arc(angle=PI/2) #creates quarter cirle
+        geod.move_to([-0.5,-0.5,0]) # moves to bottom left, but why 0.5??????
+
+        self.play(Create(circle1))
+        self.play(Create(geod))
+
