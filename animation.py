42847b588c3160c90da76cb190bb84eb544844b1
Paulina Hering <paulina.hering@outlook.de>
2021-10-24 12:23:23 +0200

    Pavels Codestylevorstellungen

--- animation.py
+++ animation.py
@@ -18,28 +18,30 @@ class CirclesNextToEachOther(Scene):
     def construct(self):
         circle1 = Circle()
         circle2 = Circle()
-        circle2.move_to([2, 0, 0]) #moves circle2 to the right by 2
+        circle2.move_to([2, 0, 0])  # moves circle2 to the right by 2
 
         self.play(Create(circle1))
         self.play(Create(circle2))
 
+
 class CircleAndGeodesic(Scene):
     def construct(self):
         circle1 = Circle()
-        line1 = Line() #creates line
-        geod = Arc(angle=PI/2) #creates quarter cirle
-        geod.move_to([-0.5,-0.5,0]) # moves to bottom left, but why 0.5??????
+        line1 = Line()  # creates line
+        geod = Arc(angle=PI / 2)  # creates quarter circle
+        geod.move_to([-0.5, -0.5, 0])  # moves to bottom left, but why 0.5??????
 
         self.play(Create(circle1))
         self.play(Create(line1))
         self.play(Create(geod))
 
+
 class Horodisk(Scene):
     def construct(self):
-        circle1 = Circle(color = WHITE, radius = 2)
-        circle2 = Circle(color = WHITE, radius = 1.5)
-        circle3 = Circle(color = WHITE, radius = 1)
-        circle4 = Circle(color = WHITE, radius = 0.5)
+        circle1 = Circle(color=WHITE, radius=2)
+        circle2 = Circle(color=WHITE, radius=1.5)
+        circle3 = Circle(color=WHITE, radius=1)
+        circle4 = Circle(color=WHITE, radius=0.5)
         dot = Dot()
 
         self.play(Create(circle1))
@@ -48,11 +50,12 @@ class Horodisk(Scene):
         self.play(Create(circle4))
         self.play(Create(dot))
 
-    #moving horodisks and dot to the right
-        line1 = Line(start = ORIGIN, end = [2,0,0] ) #to coordinate moving of horodisks and point
-        line2 = Line(start = ORIGIN, end = [0.5,0,0])
-        line3 = Line(start = ORIGIN, end = [1,0,0])
-        line4 = Line(start = ORIGIN, end = [1.5,0,0])
+        # moving horodisks and dot to the right
+        line1 = Line(start=ORIGIN, end=[2, 0, 0])  # to coordinate moving of horodisks and point
+        line2 = Line(start=ORIGIN, end=[0.5, 0, 0])
+        line3 = Line(start=ORIGIN, end=[1, 0, 0])
+        line4 = Line(start=ORIGIN, end=[1.5, 0, 0])
 
-        self.play(MoveAlongPath(dot, line1),MoveAlongPath(circle2, line2),MoveAlongPath(circle3, line3),MoveAlongPath(circle4, line4)) #moves dot to the right
-
+        # moves everything to the right
+        self.play(MoveAlongPath(dot, line1), MoveAlongPath(circle2, line2), MoveAlongPath(circle3, line3),
+                  MoveAlongPath(circle4, line4))
