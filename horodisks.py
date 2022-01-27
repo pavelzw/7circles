463672c2509ca7dfe1ad096b31d0d614ec051691
Paulina Hering <paulina.hering@outlook.de>
2022-01-27 11:46:26 +0100

    work on first scene, horodisk

--- horodisks.py
+++ horodisks.py
@@ -1,10 +1,11 @@
 from math import pi
+from manim import *
 
 import numpy as np
 from manim import Scene, Square, Circle, Dot, Group, Text, Create, FadeIn, FadeOut, MoveAlongPath, Line, WHITE, BLUE, \
     GREEN_B, Transform, MovingCameraScene, Uncreate, \
     VGroup, DecimalNumber, RIGHT, Tex, LEFT, UP, MathTex, Write, Indicate, TransformFromCopy, RED, \
-    DOWN, GREY_B, ORANGE, ArcBetweenPoints, BLUE_B, Rotate
+    DOWN, GREY_B, ORANGE, ArcBetweenPoints, BLUE_B, Rotate, NumberPlane, YELLOW, always_redraw, MovingCamera
 
 from geometry_util import polar_to_point, hyperbolic_distance_function, create_min_circle_radius, moving_circle, \
     moving_line, get_intersection_in_unit_circle_of_two_tangent_circles
@@ -14,12 +15,13 @@ from hyperbolic_polygon import HyperbolicPolygon, HyperbolicArcBetweenPoints
 
 class Scene1(MovingCameraScene):
     def construct(self):
-        self.camera.frame.width = 7.5
+        self.camera.frame.width = 8
         def_ball_hyp = MathTex(r'z:\mathrm{dist}(z,z_0)\leq r')
         def_dist_eukl = MathTex(r'\mathrm{dist}(a,b)=')
-        title_hyp = Tex(r'Hyperbolischer Raum', font_size=25, stroke_width=.5).move_to([2, 1.5, 0])
-        subtitle_hyp = Tex(r'Poincar\'{e}-Modell', font_size=18, stroke_width=.5).move_to([2, 1.3, 0])
-        title_eucl = Tex(r'Euklidischer Raum', font_size=25, stroke_width=.5).move_to([-2, 1.5, 0])
+        title_hyp = Tex(r'Hyperbolischer Raum', font_size=25, stroke_width=.5).move_to([2, 1.7, 0])
+        subtitle_hyp = Tex(r'Poincar\'{e}-Modell', font_size=18, stroke_width=.5).move_to([2, 1.5, 0])
+        title_eucl = Tex(r'Euklidischer Raum', font_size=25, stroke_width=.5).move_to([-2, 1.7, 0])
+        self.add_foreground_mobjects(title_eucl)
         separating_line = Line(start=[0, 8, 0], end=[0, -8, 0], stroke_width=2)
         self.play(Write(title_eucl))
         self.play(FadeIn(separating_line))
@@ -29,48 +31,61 @@ class Scene1(MovingCameraScene):
         # euklidischer fall
         eucl_dot = Dot([-2, 0, 0], color=WHITE, radius=.03, stroke_width=2)
         eucl_dot_tex = MathTex(r'P', font_size=20).move_to([-1.9, 0.1, 0])
-        eucl_circle = Circle(arc_center=[-2, 0, 0], radius=.75, color=WHITE, stroke_width=2)
+        eucl_circles = [Circle(arc_center=[-2, 0, 0], color=BLUE_A, stroke_width=2, radius=.25),
+                        Circle(arc_center=[-2, 0, 0], color=BLUE, stroke_width=2, radius=.5),
+                        Circle(arc_center=[-2, 0, 0], color=BLUE_E, stroke_width=2, radius=.75)]
+        eucl_mov_points = np.array([[-2, 0, 0], [-2.5, 0, 0], [-3, 0, 0], [-1.5, 0, 0]])
         radius = Line(start=[-2, 0, 0], end=[-2.75, 0, 0], color=RED, stroke_width=2)
+        grid = NumberPlane(x_range=[-8, -2, .25], y_range=[1, 9, .25], background_line_style={
+            "stroke_color": GREY, "stroke_width": .5}).move_to([-3, 0, 0])
+
+        # euklidische situation
+        self.play(FadeIn(grid))
         self.play(FadeIn(eucl_dot), FadeIn(eucl_dot_tex))
-        self.play(Create(eucl_circle))
+        self.play(Create(eucl_circles[2]))
         self.play(Create(radius))
+        # todo moving radius https://docs.manim.community/en/stable/examples.html?highlight=grid
+        self.play(FadeOut(radius))
+        self.play(Create(eucl_circles[0]), Create(eucl_circles[1]))
+        circle_radius_group = VGroup(*eucl_circles, eucl_dot, eucl_dot_tex)
         self.wait(2)
-        # todo updating moving radius
 
         # hyperbolische situation
         center = [2, 0, 0]
         start_points = np.array([center, center, center, center])
         length = [1 / 2, 1 / 2, -3 / 2]  # 1 is 1 unit to the left, -3 is 3 units to the right, way of circles moving
         angles = [0, pi / 4, 4 * pi / 3]
-
         outer_circle = Circle(color=WHITE, radius=1).move_to(center)
-        circle = [Dot(color=BLUE, radius=0.04), Circle(color=WHITE, radius=0.25, stroke_width=2),
-                  Circle(color=WHITE, radius=.5, stroke_width=2), Circle(color=WHITE, radius=0.75, stroke_width=2)]
+        self.add_foreground_mobjects(outer_circle)
+        circle = [Dot(color=WHITE, radius=0.04), Circle(color=BLUE_A, radius=0.25, stroke_width=2),
+                  Circle(color=BLUE, radius=.5, stroke_width=2), Circle(color=BLUE_E, radius=0.75, stroke_width=2)]
         circles = Group(circle[0], circle[1], circle[2], circle[3]).move_to(center)
-        # circle[0] is dot, circle[1] is biggest circle, circle[2] is middle circle, circle[3] is smallest circle
-
-        # creating circles and dot
+        # circle[0] is dot, circle[1] is smallest circle, circle[2] is middle circle, circle[3] is biggest circle
         self.play(FadeIn(outer_circle))
         self.play(FadeIn(circle[0]))
         self.wait(duration=2)
         self.play(Create(circle[1]), Create(circle[2]), Create(circle[3]))
 
         # circles moving along a line 3 times
-        for i in range(3):
-            end_points = np.array([start_points[0] - [1 * length[i], 0, 0],
-                                   start_points[1] - [0.75 * length[i], 0, 0],
-                                   start_points[2] - [0.5 * length[i], 0, 0],
-                                   start_points[3] - [0.25 * length[i], 0, 0]])
+        for t in range(3):
+            end_points = np.array([start_points[0] - [1 * length[t], 0, 0],
+                                   start_points[1] - [0.75 * length[t], 0, 0],
+                                   start_points[2] - [0.5 * length[t], 0, 0],
+                                   start_points[3] - [0.25 * length[t], 0, 0]])
             lines = moving_line(start_points, end_points)
-            self.play(MoveAlongPath(circle[0], lines[0]), MoveAlongPath(circle[3], lines[3]),
+            self.play(MoveAlongPath(circle_radius_group, Line(start=eucl_mov_points[t], end=eucl_mov_points[t + 1])),
+                      # euclidean
+                      MoveAlongPath(circle[0], lines[0]), MoveAlongPath(circle[3], lines[3]),  # hyperbolic
                       MoveAlongPath(circle[2], lines[2]), MoveAlongPath(circle[1], lines[1]), run_time=2)
             self.wait(duration=1)
             start_points = end_points
 
         # circles moving along part circle twice
-        for i in range(2):
-            arcs = moving_circle(angles[i], angles[i + 1], center)
-            self.play(MoveAlongPath(circle[0], arcs[0]), MoveAlongPath(circle[3], arcs[1]),
+        for t in range(2):
+            arcs = moving_circle(angles[t], angles[t + 1], center)
+            self.play(MoveAlongPath(circle_radius_group,  # euclidean
+                                    Arc(start_angle=angles[t], angle=angles[t + 1], arc_center=[-2, 0, 0], radius=.5)),
+                      MoveAlongPath(circle[0], arcs[0]), MoveAlongPath(circle[3], arcs[1]),
                       MoveAlongPath(circle[2], arcs[2]), MoveAlongPath(circle[1], arcs[3]), run_time=2)
             self.wait(duration=1)
 
